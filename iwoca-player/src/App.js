c3706e9d29d7465375d2e5a43617bfcd7f7d59c7
mreve <aadymek@gmail.com>
2017-12-06 15:07:35 +0000

    Setup basic React JS app, fix titles

---
+++ iwoca-player/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-logo">♬</div>
+          <h1 className="App-title">iwoca player</h1>
+        </header>
+        <p>Hello, iwoca!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
